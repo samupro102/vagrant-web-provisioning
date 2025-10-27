f23b29b03048fcefbf624c2340579dd113444fc3
Samuel <samuelarboleda39@gmail.com>
2025-10-27 16:48:18 -0500

    Proyecto Vagrant Web + DB - Primera versión

--- provision-db.sh
+++ provision-db.sh
@@ -1,1 +1,17 @@
-#debes completar este archivo con los comandos necesarios para provisionar la base de datos
+#!/usr/bin/env bash
+# Script de aprovisionamiento para PostgreSQL
+
+# Actualiza los repositorios
+sudo apt update -y
+
+# Instala PostgreSQL
+sudo apt install -y postgresql postgresql-contrib
+
+# Inicia y habilita el servicio
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
+
+# Crea base de datos y usuario de ejemplo
+sudo -u postgres psql -c "CREATE DATABASE ejemplo_db;"
+sudo -u postgres psql -c "CREATE USER samuel WITH ENCRYPTED PASSWORD '1234';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ejemplo_db TO samuel;"
